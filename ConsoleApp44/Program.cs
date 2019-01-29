ee9dc16ea289c6068c09c39ce0e349693e9692c2
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-01-29 16:39:30 +0100

    Add project files.

---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            double Cup = 450;
+            double velocity = Getspeed();
+            double angle = Getangle();
+            double strokes = 1; //Getstrokes();
+            double balllength = GetLength(angle, velocity);
+            double lengTotoCup = Cup - balllength;
+
+            while (strokes < 10 && lengTotoCup < Cup)
+            {
+
+
+
+
+                {
+
+
+                    Console.WriteLine("Ball traveld: " + balllength);
+                    Console.WriteLine("Left to cup: " + lengTotoCup);
+                    Console.ReadKey();
+
+
+                    strokes = strokes + 1;
+                    velocity = Getspeed();
+                    angle = Getangle();
+                    balllength = GetLength(angle, velocity);
+                    lengTotoCup = lengTotoCup - balllength;
+
+                }
+            }
+
+
+        }
+
+
+
+        public static double Getspeed()
+        {
+            Console.Write("write velocityt");
+            double velocity = double.Parse(Console.ReadLine());
+            return velocity;
+        }
+
+        public static double Getangle()
+        {
+
+
+            Console.Write("write angle");
+
+            double angle = double.Parse(Console.ReadLine());
+            return angle;
+        }
+        public static double GetLength(double angle, double velocity)
+        {
+            double length;
+
+
+
+            double angleRad = (Math.PI / 180) * angle;
+            // Distance: Math.Pow(velocity, 2) / GRAVITY * Math.Sin(2 * angleInRadians)
+            length = Math.Pow(velocity, 2) / 9.8 * Math.Sin(2 * angleRad);
+
+
+            return length;
+
+        }
+
+        private static double Getstrokes()
+        {
+            {
+
+                double strokes = double.Parse(Console.ReadLine());
+                strokes = strokes + strokes;
+                return strokes;
+
+
+            }
+        }
+
+
+    }
+
+}
+
