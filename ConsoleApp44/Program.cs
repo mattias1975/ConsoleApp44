4f7cb5f17bc412afec40854c5de8be5a6a5dfb6a
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-01-30 14:27:20 +0100

    tagit bort try ctach och lagt medelande om man har slut på slag och ett om man kommit i hållet

--- ConsoleApp44/Program.cs
+++ ConsoleApp44/Program.cs
@@ -50,8 +50,7 @@ namespace ConsoleApp43
                         }
                                                 
                                         }
-
-                              
+                                                  
                   
 
             }
